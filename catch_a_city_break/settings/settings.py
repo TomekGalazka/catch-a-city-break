a1fbb37d245a4b7a7f3fb5ee4bb78824c73cbbd8
Tomasz Gałązka <tom3k.galazka@gmail.com>
2021-01-04 21:26:01 +0100

    Security fix.

--- catch_a_city_break/settings/settings.py
+++ catch_a_city_break/settings/settings.py
@@ -12,15 +12,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from .secret_settings import *
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x*_da&y%u1cypbd2v3y&$i4+pee@=jxmc)-m+0b678v4d4jv-+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
